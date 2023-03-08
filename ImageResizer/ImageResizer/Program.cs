b47f39e4e25c9138abeb5b3534b815ca87a3fa46
ElfinWeb <mr.hawl1227@gmail.com>
2023-03-08 17:44:52 +0300

    030820231744P

--- ImageResizer/ImageResizer/Program.cs
+++ ImageResizer/ImageResizer/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using ImageResizer;
+
+string fileName = Guid.NewGuid().ToString() + ".jpg";
+string fullPath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+Console.WriteLine("Enter the height :");
+string height = Console.ReadLine();
+
+Console.WriteLine("Enter the width :");
+string width = Console.ReadLine();
+
+try
+{
+    using (var stream = File.OpenRead(fullPath))
+    {
+        Image img = Image.Load(stream);
+        string newSize = Resizer.Resize(img, int.Parse(height), int.Parse(width));
+        string[] aSize = newSize.Split(",");
+
+        img.Mutate(i => i.Resize(int.Parse(aSize[1]), int.Parse(aSize[0])));
+
+        img.Save(fullPath);
+    }
+    Console.WriteLine("Image resized!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
