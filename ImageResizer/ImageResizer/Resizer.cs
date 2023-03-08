3ef265b36971fbf5113ec84ac096ade8d3187984
ElfinWeb <mr.hawl1227@gmail.com>
2023-03-08 17:26:45 +0300

    030820231726P

--- ImageResizer/ImageResizer/Resizer.cs
+++ ImageResizer/ImageResizer/Resizer.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ImageResizer
 {
-    internal class Resizer
+    public static class Resizer
     {
+        public static string Resize(Image img, int maxHeight, int maxWidth)
+        {
+            if (img.Width > maxWidth || img.Height > maxHeight)
+            {
+                double widthRatio = (double)img.Width / maxWidth;
+                double heightRatio = (double)img.Height / maxHeight;
+                double ratio = Math.Max(widthRatio, heightRatio);
+
+                int newWidth = (int)(img.Width / ratio);
+                int newHeight = (int)(img.Height / ratio);
+                return newHeight.ToString() + "," + newWidth.ToString();
+            }
+            else
+            {
+                return img.Height.ToString() + "," + img.Width.ToString();
+            }
+        }
     }
 }
